0e04ba16270722ec3a22032ebb1f217d0b90cf06
Nazmus Sakib <your-email@example.com>
2025-08-16 20:40:26 +0600

    map

--- src/app/(withCommonLayout)/contact/page.tsx
+++ src/app/(withCommonLayout)/contact/page.tsx
@@ -95,7 +95,7 @@ const page = () => {
             loading="lazy"
             allowFullScreen
             referrerPolicy="no-referrer-when-downgrade"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.848280008054!2d90.39124631498154!3d23.75088508458933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8915512f071%3A0x99fa2a85c09ccfe3!2sMirpur%201%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1720789410664!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.205808536635!2d90.41954157500507!3d23.846824478604745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c700094f80c7%3A0x387310a2dcde21ec!2sFabTech.IT!5e0!3m2!1sen!2sbd!4v1755354577039!5m2!1sen!2sbd"
           ></iframe>
         </div>
       </div>
